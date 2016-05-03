69e525ff18249c96d55c3bf9fadba8b2d14f5d0a
alexjhannan <alexjhannan@gmail.com>
2016-05-03 15:58:17 -0400

    Comments, change location button

--- client/components/weatherWidget.jsx
+++ client/components/weatherWidget.jsx
@@ -28,10 +28,69 @@ export default WeatherWidget = React.createClass({
 				<h4 className="weatherWidget--location">{"Latitude " + this.props.weather.latitude.toPrecision(4) + ", Longitude " + this.props.weather.longitude.toPrecision(4)}</h4>
 				<hr />
 				{renderContent()}
+				<button className="btn btn-primary">Change Location</button>
 			</div>
 		)
 	},
-	renderBlock(mainHeader, header1, temp1, header2, temp2, icon) {
+	renderCurrent() {
+		// current weather
+		let weather = this.props.weather;
+
+		return (
+			<div>
+				{this.renderBlock("currently", "actual", weather.currently.temperature, "feels", weather.currently.apparentTemperature, weather.currently.icon)}
+				<hr />
+				{this.renderBlock("next hour", "actual", weather.hourly.data[1].temperature, "feels", weather.hourly.data[1].apparentTemperature, weather.hourly.data[1].icon)}
+				<hr />
+				{this.renderBlock("tomorrow", "min", weather.daily.data[1].temperatureMin, "max", weather.daily.data[1].temperatureMax, weather.daily.data[1].icon)}
+				<div className="row">
+					<button onClick={this.changeState.bind(null, "fiveDay")} className="weatherWidget--button col-xs-6 btn btn-primary">Five Day</button>
+					<button onClick={this.changeState.bind(null, "sevenDay")} className="weatherWidget--button col-xs-6 btn btn-primary">Seven Day</button>
+				</div>
+			</div>
+		)
+	},
+	renderFiveDay() {
+		// five day forecast
+		let weather = this.props.weather;
+
+		return (
+			<div>
+				{this.renderBlock(moment(weather.daily.data[0].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[0].temperatureMin, "max", weather.daily.data[0].temperatureMax, weather.daily.data[0].icon)}
+				{this.renderBlock(moment(weather.daily.data[1].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[1].temperatureMin, "max", weather.daily.data[1].temperatureMax, weather.daily.data[1].icon)}
+				{this.renderBlock(moment(weather.daily.data[2].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[2].temperatureMin, "max", weather.daily.data[2].temperatureMax, weather.daily.data[2].icon)}
+				{this.renderBlock(moment(weather.daily.data[3].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[3].temperatureMin, "max", weather.daily.data[3].temperatureMax, weather.daily.data[3].icon)}
+				{this.renderBlock(moment(weather.daily.data[4].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[4].temperatureMin, "max", weather.daily.data[4].temperatureMax, weather.daily.data[4].icon)}
+				<div className="row">
+					<button onClick={this.changeState.bind(null, "current")} className="weatherWidget--button col-xs-6 btn btn-primary">Current</button>
+					<button onClick={this.changeState.bind(null, "sevenDay")} className="weatherWidget--button col-xs-6 btn btn-primary">Seven Day</button>
+				</div>
+			</div>
+		)
+	},
+	renderSevenDay() {
+		// seven day forecast
+		let weather = this.props.weather;
+
+		return (
+			<div>
+				{this.renderBlock(moment(weather.daily.data[0].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[0].temperatureMin, "max", weather.daily.data[0].temperatureMax, weather.daily.data[0].icon)}
+				{this.renderBlock(moment(weather.daily.data[1].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[1].temperatureMin, "max", weather.daily.data[1].temperatureMax, weather.daily.data[1].icon)}
+				{this.renderBlock(moment(weather.daily.data[2].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[2].temperatureMin, "max", weather.daily.data[2].temperatureMax, weather.daily.data[2].icon)}
+				{this.renderBlock(moment(weather.daily.data[3].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[3].temperatureMin, "max", weather.daily.data[3].temperatureMax, weather.daily.data[3].icon)}
+				{this.renderBlock(moment(weather.daily.data[4].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[4].temperatureMin, "max", weather.daily.data[4].temperatureMax, weather.daily.data[4].icon)}
+				{this.renderBlock(moment(weather.daily.data[5].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[5].temperatureMin, "max", weather.daily.data[5].temperatureMax, weather.daily.data[5].icon)}
+				{this.renderBlock(moment(weather.daily.data[6].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[6].temperatureMin, "max", weather.daily.data[6].temperatureMax, weather.daily.data[6].icon)}
+				
+				<div className="row">
+					<button onClick={this.changeState.bind(null, "current")} className="weatherWidget--button col-xs-6 btn btn-primary">Current</button>
+					<button onClick={this.changeState.bind(null, "fiveDay")} className="weatherWidget--button col-xs-6 btn btn-primary">Five Day</button>
+				</div>
+			</div>
+		)
+	},
+	renderBlock(mainHeader, header1, temp1, header2, temp2, icon) {	
+		// renders a single block, to represent a single data point
 		return (
 			<div>
 				<h4 className="weatherWidget--category">{mainHeader}</h4>
@@ -52,58 +111,4 @@ export default WeatherWidget = React.createClass({
 			</div>
 		)
 	},
-	renderCurrent() {
-		let weather = this.props.weather;
-
-		return (
-			<div>
-				{this.renderBlock("currently", "actual", weather.currently.temperature, "feels", weather.currently.apparentTemperature, weather.currently.icon)}
-				<hr />
-				{this.renderBlock("next hour", "actual", weather.hourly.data[1].temperature, "feels", weather.hourly.data[1].apparentTemperature, weather.hourly.data[1].icon)}
-				<hr />
-				{this.renderBlock("tomorrow", "min", weather.daily.data[1].temperatureMin, "max", weather.daily.data[1].temperatureMax, weather.daily.data[1].icon)}
-				<div className="row">
-					<button onClick={this.changeState.bind(null, "fiveDay")} className="weatherWidget--button col-xs-6 btn btn-primary">Five Day</button>
-					<button onClick={this.changeState.bind(null, "sevenDay")} className="weatherWidget--button col-xs-6 btn btn-primary">Seven Day</button>
-				</div>
-			</div>
-		)
-	},
-	renderFiveDay() {
-		let weather = this.props.weather;
-
-		return (
-			<div>
-				{this.renderBlock(moment(weather.daily.data[0].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[0].temperatureMin, "max", weather.daily.data[0].temperatureMax, weather.daily.data[0].icon)}
-				{this.renderBlock(moment(weather.daily.data[1].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[1].temperatureMin, "max", weather.daily.data[1].temperatureMax, weather.daily.data[1].icon)}
-				{this.renderBlock(moment(weather.daily.data[2].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[2].temperatureMin, "max", weather.daily.data[2].temperatureMax, weather.daily.data[2].icon)}
-				{this.renderBlock(moment(weather.daily.data[3].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[3].temperatureMin, "max", weather.daily.data[3].temperatureMax, weather.daily.data[3].icon)}
-				{this.renderBlock(moment(weather.daily.data[4].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[4].temperatureMin, "max", weather.daily.data[4].temperatureMax, weather.daily.data[4].icon)}
-				<div className="row">
-					<button onClick={this.changeState.bind(null, "current")} className="weatherWidget--button col-xs-6 btn btn-primary">Current</button>
-					<button onClick={this.changeState.bind(null, "sevenDay")} className="weatherWidget--button col-xs-6 btn btn-primary">Seven Day</button>
-				</div>
-			</div>
-		)
-	},
-	renderSevenDay() {
-		let weather = this.props.weather;
-
-		return (
-			<div>
-				{this.renderBlock(moment(weather.daily.data[0].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[0].temperatureMin, "max", weather.daily.data[0].temperatureMax, weather.daily.data[0].icon)}
-				{this.renderBlock(moment(weather.daily.data[1].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[1].temperatureMin, "max", weather.daily.data[1].temperatureMax, weather.daily.data[1].icon)}
-				{this.renderBlock(moment(weather.daily.data[2].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[2].temperatureMin, "max", weather.daily.data[2].temperatureMax, weather.daily.data[2].icon)}
-				{this.renderBlock(moment(weather.daily.data[3].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[3].temperatureMin, "max", weather.daily.data[3].temperatureMax, weather.daily.data[3].icon)}
-				{this.renderBlock(moment(weather.daily.data[4].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[4].temperatureMin, "max", weather.daily.data[4].temperatureMax, weather.daily.data[4].icon)}
-				{this.renderBlock(moment(weather.daily.data[5].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[5].temperatureMin, "max", weather.daily.data[5].temperatureMax, weather.daily.data[5].icon)}
-				{this.renderBlock(moment(weather.daily.data[6].time.toString(), "X").format("ddd MMM Do"), "min", weather.daily.data[6].temperatureMin, "max", weather.daily.data[6].temperatureMax, weather.daily.data[6].icon)}
-				
-				<div className="row">
-					<button onClick={this.changeState.bind(null, "current")} className="weatherWidget--button col-xs-6 btn btn-primary">Current</button>
-					<button onClick={this.changeState.bind(null, "fiveDay")} className="weatherWidget--button col-xs-6 btn btn-primary">Five Day</button>
-				</div>
-			</div>
-		)
-	}
 });
